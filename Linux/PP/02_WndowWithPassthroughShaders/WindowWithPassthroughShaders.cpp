7fb1b8c86923d444f1610b9cb56b68c40b22e226
Jayshree <jayshreeggandhi@gmail.com>
2019-10-04 23:49:51 +0530

    XWindows Pp assignments

---
+++ Linux/PP/02_WndowWithPassthroughShaders/WindowWithPassthroughShaders.cpp
@@ -0,0 +1,726 @@
+#include<iostream> 
+#include<stdio.h>
+#include<stdlib.h>
+#include<memory.h>
+
+#include<X11/Xlib.h>
+#include<X11/Xutil.h>
+#include<X11/XKBlib.h>
+#include<X11/keysym.h>
+
+#include<GL/glew.h>
+#include<GL/gl.h>
+#include<GL/glx.h>
+
+#include"vmath.h"
+
+using namespace std;
+using namespace vmath;
+
+Window gWindow;
+Colormap gColormap;
+XVisualInfo gVisualInfo;
+Display *gpDisplay = NULL;
+XVisualInfo *gpXVisualInfo = NULL;
+
+bool bFullscreen = false;
+int gWindowWidth = 800;
+int gWindowHeight = 600;
+FILE *gpFile = NULL;
+
+typedef GLXContext (*glXCreateContextAttribsARBProc) (Display *, GLXFBConfig,
+GLXContext, Bool, const int *);
+glXCreateContextAttribsARBProc glXCreateContextAttribsARB = NULL;
+
+GLXFBConfig gGLXFBConfig;
+static GLXContext gGLXContext;
+bool bDone = false;
+
+GLuint gVertexShaderObject;
+GLuint gFragmentShaderObject;
+GLuint gShaderProgramObject;
+
+
+
+//function prototypes
+void CreateWindow(void);
+void ToggleFullscreen(void);
+void uninitialize(void);
+
+void initialize(void);
+void resize(int,int);
+void display(void);
+//void update(void);
+
+int main(void)
+{
+
+	int winWidth = gWindowWidth;
+	int winHeight = gWindowHeight;
+
+	gpFile = fopen("Log.txt", "w"); 
+	if (!(gpFile))
+	{
+		printf("Log file can not be created\n");
+		exit(0);
+	}
+	else
+	{
+		fprintf(gpFile, "Log file created successfully...\n");
+	}
+
+	CreateWindow();
+
+	initialize();
+
+	XEvent event;
+	KeySym keysym;
+
+	while(bDone == false)
+	{
+		while(XPending(gpDisplay))
+		{
+			XNextEvent(gpDisplay,&event);
+
+			switch(event.type)
+			{
+				case MapNotify:
+					break;
+
+				case KeyPress:
+					keysym = XkbKeycodeToKeysym(gpDisplay, event.xkey.keycode, 0, 0);
+					switch(keysym)
+					{
+						case XK_Escape:
+							bDone = true;
+							break;
+
+						case XK_F:
+						case XK_f:
+							if(bFullscreen == false)
+							{
+								ToggleFullscreen();
+								bFullscreen = true;
+							}
+							else
+							{
+								ToggleFullscreen();
+								bFullscreen = false;
+							}
+							break;
+
+						default:
+							break;
+					}
+					break;
+
+				case ButtonPress:
+					switch(event.xbutton.button)
+					{
+						case 1:
+							break;
+
+						case 2:
+							break;
+
+						case 3:
+							break;
+
+						default:
+							break;
+					}
+					break;
+
+				case MotionNotify:
+					break;
+
+				case ConfigureNotify:
+					winWidth = event.xconfigure.width;
+					winHeight = event.xconfigure.height;
+
+					resize(winWidth,winHeight);
+					break;
+
+				case Expose:
+					break;
+
+				case DestroyNotify:
+					break;
+
+				case 33:
+					bDone = true;
+					break;
+
+				default:
+					break;
+			}
+		}
+
+		//update();
+		display();
+	}
+
+	uninitialize();
+	return(0);
+}
+
+void CreateWindow(void)
+{
+	XSetWindowAttributes winAttribs;
+	int defaultScreen;
+	int styleMask;
+
+	GLXFBConfig *pGLXFBConfig = NULL;
+	GLXFBConfig bestGLXFBConfig;
+	XVisualInfo *pTempXVisualInfo = NULL;
+	int iNumberOfFBConfigs = 0;
+
+	//though not necessary in our opengl practices , it(static) is done by many SDKs ex: Imagination,Unreal 
+	static int frameBufferAttribute[] = {
+		GLX_X_RENDERABLE, True,
+		GLX_DRAWABLE_TYPE, GLX_WINDOW_BIT,
+		GLX_RENDER_TYPE, GLX_RGBA_BIT,
+		GLX_X_VISUAL_TYPE, GLX_TRUE_COLOR,
+		GLX_RED_SIZE, 8,
+		GLX_GREEN_SIZE, 8,
+		GLX_BLUE_SIZE, 8,
+		GLX_ALPHA_SIZE, 8,
+		GLX_DEPTH_SIZE,24,
+		GLX_STENCIL_SIZE, 8,
+		GLX_DOUBLEBUFFER,True,
+		None
+	};
+
+	
+	gpDisplay = XOpenDisplay(NULL);
+	if(gpDisplay == NULL)
+	{
+		fprintf(gpFile, "\nERROR : Unable to open XDisplay.Exitting now..\n");
+		printf("\nERROR : Unable to open XDisplay.\nExitting now..\n");
+		uninitialize();
+		exit(1);
+	}
+
+	defaultScreen = XDefaultScreen(gpDisplay);
+
+	//reterive all FBConfigs driver has
+	pGLXFBConfig = glXChooseFBConfig(gpDisplay, defaultScreen, frameBufferAttribute, &iNumberOfFBConfigs);
+
+	printf("\nThere are %d matching FBConfigs\n", iNumberOfFBConfigs);
+
+	int bestFrameBufferConfig = -1;
+	int bestNumberOfSamples = -1;
+	int worstFrameBufferConfig = -1;
+	int worstNumberOfSamples = 999;
+
+	int sampleBuffers = 0;
+	int samples = 0;
+
+	for(int i = 0; i < iNumberOfFBConfigs; i++)
+	{
+		
+		//for each obtained FBCongig, get temp visual info
+		pTempXVisualInfo = glXGetVisualFromFBConfig(gpDisplay, pGLXFBConfig[i]);
+		if(pTempXVisualInfo)
+		{
+		
+			//get numbers of sample buffers from respective FBConfig
+			glXGetFBConfigAttrib(gpDisplay, pGLXFBConfig[i], GLX_SAMPLE_BUFFERS, &sampleBuffers);
+			
+			//get number of samples from respective FBConfig
+			glXGetFBConfigAttrib(gpDisplay, pGLXFBConfig[i], GLX_SAMPLES, &samples);
+			
+			//More the number of samples and sample buffers, more the eligible FBConfig is.So do the comparision
+			//get best from all
+			if(bestFrameBufferConfig < 0 || (sampleBuffers && (samples > bestNumberOfSamples)))
+			{
+			
+				bestFrameBufferConfig = i;
+				bestNumberOfSamples = samples;
+			} 
+
+			//get worst from all
+			if(worstFrameBufferConfig < 0 || (!sampleBuffers) || (samples < worstNumberOfSamples))
+			{
+				worstFrameBufferConfig = i;
+				worstNumberOfSamples = samples;
+			}
+		}
+		
+		//free pTempXVisualInfo
+		XFree(pTempXVisualInfo);
+	}
+	
+	//Assign the best found
+	bestGLXFBConfig = pGLXFBConfig[bestFrameBufferConfig];
+	
+	//assign the same to global one
+	gGLXFBConfig = bestGLXFBConfig;
+	
+	//Free the obtained glXFBConfig;
+	XFree(pGLXFBConfig);
+	
+	//accordingly get the best visual
+	gpXVisualInfo = glXGetVisualFromFBConfig(gpDisplay, bestGLXFBConfig);
+	
+	/*
+	//Bridging API gives you visual in opengl code
+	gpXVisualInfo = glXChooseVisual(gpDisplay, defaultScreen, frameBufferAttribute);
+	
+
+	if(gpXVisualInfo == NULL)
+	{
+		printf("\n18");
+		fprintf(gpFile, "\nERROR : Unable to get XVisualInfo .Exitting now..\n");
+		printf("\nERROR : Unable to get XVisualInfo.\nExitting now..\n");
+		uninitialize();
+		exit(1);
+	}
+	*/
+
+	winAttribs.border_pixel = 0;
+	winAttribs.border_pixmap = 0;
+	winAttribs.colormap = XCreateColormap(gpDisplay, 
+			RootWindow(gpDisplay, gpXVisualInfo->screen), 
+			gpXVisualInfo->visual, 
+			AllocNone);
+	winAttribs.background_pixel = BlackPixel(gpDisplay, defaultScreen);
+	winAttribs.background_pixmap = 0;
+	winAttribs.event_mask = ExposureMask | 
+		VisibilityChangeMask |
+		ButtonPressMask | 
+		KeyPressMask |
+		PointerMotionMask |
+		StructureNotifyMask;
+
+	styleMask = CWBorderPixel |
+		CWBackPixel |
+		CWEventMask |
+		CWColormap;
+
+	gWindow = XCreateWindow(gpDisplay,
+			RootWindow(gpDisplay, gpXVisualInfo->screen),
+			0,
+			0,
+			gWindowWidth,
+			gWindowHeight,
+			0,
+			gpXVisualInfo->depth,
+			InputOutput,
+			gpXVisualInfo->visual,
+			styleMask,
+			&winAttribs);
+
+	if(!gWindow)
+	{	
+		fprintf(gpFile, "\nERROR : Failed to create main window.Exitting now..\n");
+		printf("\nERROR : Failed to create main window.\nExitting now..\n");
+		uninitialize();
+		exit(1);
+	}
+
+	XStoreName(gpDisplay, gWindow, "My First XWindows PP Window - Jayshree Gandhi");
+
+	Atom windowManagerDelete = XInternAtom(gpDisplay, "WM_DELETE_WINDOW",True);
+	XSetWMProtocols(gpDisplay, gWindow, &windowManagerDelete, 1);
+
+	XMapWindow(gpDisplay, gWindow);
+}
+
+
+void ToggleFullscreen(void)
+{
+
+	Atom wm_state;
+	Atom fullscreen;
+	XEvent xev = {0};
+
+	wm_state = XInternAtom(gpDisplay, "_NET_WM_STATE", False);
+
+	memset(&xev, 0, sizeof(xev));
+
+	xev.type = ClientMessage;
+	xev.xclient.window = gWindow;
+	xev.xclient.message_type = wm_state;
+	xev.xclient.format = 32;
+	xev.xclient.data.l[0] = bFullscreen ? 0 : 1;
+
+
+	fullscreen = XInternAtom(gpDisplay, "_NET_WM_STATE_FULLSCREEN", False);
+	xev.xclient.data.l[1] = fullscreen;
+
+	XSendEvent(gpDisplay, 
+			RootWindow(gpDisplay, gpXVisualInfo->screen),
+			False,
+			StructureNotifyMask,
+			&xev);
+}
+
+
+void initialize(void)
+{
+	GLenum result;
+
+	//get the rendering context
+	glXCreateContextAttribsARB = (glXCreateContextAttribsARBProc)glXGetProcAddressARB((GLubyte*)"glXCreateContextAttribsARB");
+	
+	if(!glXCreateContextAttribsARB)
+	{
+		//return(-1);
+		fprintf(gpFile, "\nERROR : glXGetProcAddressARB() Failed\n");
+		printf("\nRashtrageet!");
+		printf("\nERROR : glXGetProcAddressARB() Failed\nExitting now..\n");
+		uninitialize();
+		exit(0);
+	}
+	
+	//Context attribute array
+	GLint attribs[] = {
+		GLX_CONTEXT_MAJOR_VERSION_ARB, 4,
+		GLX_CONTEXT_MINOR_VERSION_ARB, 5,
+		GLX_CONTEXT_PROFILE_MASK_ARB, GLX_CONTEXT_CORE_PROFILE_BIT_ARB,
+		None
+	};
+
+	//now get the context
+	gGLXContext = glXCreateContextAttribsARB(gpDisplay,
+		gGLXFBConfig,
+		0,
+		True,
+		attribs);
+
+	if(!gGLXContext)
+	{
+		GLint attribs[] = {
+			GLX_CONTEXT_MAJOR_VERSION_ARB, 1,
+			GLX_CONTEXT_MINOR_VERSION_ARB, 0,
+			None
+		};
+		printf("\ni8");
+		gGLXContext = glXCreateContextAttribsARB(gpDisplay,
+			gGLXFBConfig,
+			0,
+			True,
+			attribs);
+	}
+	
+	if(!glXIsDirect(gpDisplay, gGLXContext))
+	{
+		fprintf(gpFile, "\nThe obtained context is not hardware context\n");
+		printf("\nThe obtained context is not hardware context\n");
+		
+	}
+	else
+	{	fprintf(gpFile, "\nThe obtained context is a hardware context\n");
+		printf("\nThe obtained context is a hardware context\n");
+		
+	}
+	
+	glXMakeCurrent(gpDisplay,gWindow,gGLXContext);
+
+	//On the extentions 
+	result = glewInit();
+	if(result != GLEW_OK)
+	{
+		fprintf(gpFile, "\nERROR : glewInit() Failed\n");
+		printf("\nERROR : glewInit() Failed\nExitting now..\n");
+		uninitialize();
+		exit(1);
+	}
+
+	//***************** 1. VERTEX SHADER ************************************ 
+	//define vertex shader object
+	//create vertex shader object
+	gVertexShaderObject = glCreateShader(GL_VERTEX_SHADER);
+
+	//Write vertex shader code
+	const GLchar *vertexShaderSourceCode =
+		"#version 450 core" \
+		"\n" \
+		"void main(void)" \
+		"{" \
+		"}" ;
+
+	//specify above source code to vertex shader object
+	glShaderSource(gVertexShaderObject,//to whom?
+		1,//how many strings
+		(const GLchar **)&vertexShaderSourceCode,//address of string
+		NULL);// NULL specifes that there is only one string with fixed length
+
+	//Compile the vertex shader
+	glCompileShader(gVertexShaderObject);
+
+	
+	//Error checking for compilation:
+	GLint iShaderCompileStatus = 0;
+	GLint iInfoLogLength = 0;
+	GLchar *szInfoLog = NULL;
+
+	//Step 1 : Call glGetShaderiv() to get comiple status of particular shader
+	glGetShaderiv(gVertexShaderObject, // whose?
+		GL_COMPILE_STATUS,//what to get?
+		&iShaderCompileStatus);//in what?
+
+	//Step 2 : Check shader compile status for GL_FALSE
+	if (iShaderCompileStatus == GL_FALSE)
+	{
+		//Step 3 : If GL_FALSE , call glGetShaderiv() again , but this time to get info log length 
+		glGetShaderiv(gVertexShaderObject,
+			GL_INFO_LOG_LENGTH,
+			&iInfoLogLength);
+
+		//Step 4 : if info log length > 0 , call glGetShaderInfoLog()
+		if (iInfoLogLength > 0)
+		{
+			//allocate memory to pointer
+			szInfoLog = (GLchar *)malloc(iInfoLogLength);
+			if (szInfoLog != NULL)
+			{
+				GLsizei written;
+
+				glGetShaderInfoLog(gVertexShaderObject,//whose?
+					iInfoLogLength,//length?
+					&written,//might have not used all, give that much only which have been used in what?
+					szInfoLog);//store in what?
+
+				fprintf(gpFile, "\nVertex Shader Compilation Log : %s\n", szInfoLog);
+
+				//free the memory
+				free(szInfoLog);
+
+				uninitialize();
+				exit(0);
+			}
+		}
+	}
+
+
+	//************************** 2. FRAGMENT SHADER ********************************
+	//define fragment shader object
+	//create fragment shader object
+	gFragmentShaderObject = glCreateShader(GL_FRAGMENT_SHADER);
+
+	//write fragment shader code
+	const GLchar *fragmentShaderSourceCode =
+		"#version 450 core" \
+		"\n" \
+		"void main(void)" \
+		"{" \
+		"}";
+
+	//specify the above source code to fragment shader object
+	glShaderSource(gFragmentShaderObject,
+		1,
+		(const GLchar **)&fragmentShaderSourceCode,
+		NULL);
+
+	//compile the fragment shader
+	glCompileShader(gFragmentShaderObject);
+
+	
+	//Error checking for compilation
+	iShaderCompileStatus = 0;
+	iInfoLogLength = 0;
+	szInfoLog = NULL;
+
+	//Step 1 : Call glGetShaderiv() to get comiple status of particular shader
+	glGetShaderiv(gFragmentShaderObject, // whose?
+		GL_COMPILE_STATUS,//what to get?
+		&iShaderCompileStatus);//in what?
+
+	//Step 2 : Check shader compile status for GL_FALSE
+	if (iShaderCompileStatus == GL_FALSE)
+	{
+		//Step 3 : If GL_FALSE , call glGetShaderiv() again , but this time to get info log length 
+		glGetShaderiv(gFragmentShaderObject,
+			GL_INFO_LOG_LENGTH,
+			&iInfoLogLength);
+
+		//Step 4 : if info log length > 0 , call glGetShaderInfoLog()
+		if (iInfoLogLength > 0)
+		{
+			//allocate memory to pointer
+			szInfoLog = (GLchar *)malloc(iInfoLogLength);
+			if (szInfoLog != NULL)
+			{
+				GLsizei written;
+
+				glGetShaderInfoLog(gFragmentShaderObject,//whose?
+					iInfoLogLength,//length?
+					&written,//might have not used all, give that much only which have been used in what?
+					szInfoLog);//store in what?
+
+				fprintf(gpFile, "\nFragment Shader Compilation Log : %s\n", szInfoLog);
+
+				//free the memory
+				free(szInfoLog);
+
+				uninitialize();
+				exit(0);
+			}
+		}
+	}
+
+	//create shader program object
+	gShaderProgramObject = glCreateProgram();
+
+	//Attach vertex shader to shader program
+	glAttachShader(gShaderProgramObject,//to whom?
+		gVertexShaderObject);//what to attach?
+
+	//Attach fragment shader to shader program
+	glAttachShader(gShaderProgramObject,
+		gFragmentShaderObject);
+
+	//Link the shader program
+	glLinkProgram(gShaderProgramObject);//link to whom?
+
+	
+	//Error checking for linking
+	GLint iProgramLinkStatus = 0;
+	iInfoLogLength = 0;
+	szInfoLog = NULL;
+
+	//Step 1 : Call glGetProgramiv() to get comiple status of particular shader
+	glGetProgramiv(gShaderProgramObject, // whose?
+		GL_LINK_STATUS,//what to get?
+		&iProgramLinkStatus);//in what?
+
+	//Step 2 : Check shader compile status for GL_FALSE
+	if (iProgramLinkStatus == GL_FALSE)
+	{
+		//Step 3 : If GL_FALSE , call glGetShaderiv() again , but this time to get info log length 
+		glGetProgramiv(gShaderProgramObject,
+			GL_INFO_LOG_LENGTH,
+			&iInfoLogLength);
+
+		//Step 4 : if info log length > 0 , call glGetShaderInfoLog()
+		if (iInfoLogLength > 0)
+		{
+			//allocate memory to pointer
+			szInfoLog = (GLchar *)malloc(iInfoLogLength);
+			if (szInfoLog != NULL)
+			{
+				GLsizei written;
+
+				glGetProgramInfoLog(gShaderProgramObject,//whose?
+					iInfoLogLength,//length?
+					&written,//might have not used all, give that much only which have been used in what?
+					szInfoLog);//store in what?
+
+				fprintf(gpFile, "\nShader Program Linking Log : %s\n", szInfoLog);
+
+				//free the memory
+				free(szInfoLog);
+
+				uninitialize();
+				exit(0);
+			}
+		}
+	}
+	
+	//usual opengl initialization code:
+	glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+
+	
+	resize(gWindowWidth,gWindowHeight);
+	
+}
+
+void resize(int width, int height)
+{
+	//usual resize code
+	if(height == 0)
+	{
+		height = 1;
+	}
+
+	glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+	
+}
+
+void display(void)
+{
+	//usual display code - last line only in double buffering
+	//but in single buffering, whole code is same
+
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	//Binding Opengl code to shader program object
+	glUseProgram(gShaderProgramObject);
+
+
+	//unbinding
+	glUseProgram(0);
+
+	glXSwapBuffers(gpDisplay, gWindow);
+}
+
+/*void update(void)
+{
+	
+}*/
+
+void uninitialize(void)
+{
+	glUseProgram(gShaderProgramObject);
+	
+	glDetachShader(gShaderProgramObject, gFragmentShaderObject);
+
+	glDetachShader(gShaderProgramObject, gVertexShaderObject);
+
+	glDeleteShader(gFragmentShaderObject);
+	gFragmentShaderObject = 0;
+
+	glDeleteShader(gVertexShaderObject);
+	gVertexShaderObject = 0;
+
+	glDeleteProgram(gShaderProgramObject);
+	gShaderProgramObject = 0;
+
+	glUseProgram(0);
+
+	GLXContext currentGLXContext = glXGetCurrentContext();
+	if(currentGLXContext != NULL && currentGLXContext == gGLXContext)
+	{
+		glXMakeCurrent(gpDisplay, 0,0);
+	}
+
+	if(gGLXContext)
+	{
+		glXDestroyContext(gpDisplay,gGLXContext);
+	}
+	
+	if(gWindow)
+	{
+		XDestroyWindow(gpDisplay, gWindow);
+		gWindow = 0;
+	}
+
+	if(gColormap)
+	{
+		XFreeColormap(gpDisplay, gColormap);
+		gColormap = 0;
+	}
+
+	if(gpXVisualInfo)
+	{
+		free(gpXVisualInfo);
+		gpXVisualInfo = NULL;
+	}
+
+	if(gpDisplay)
+	{
+		XCloseDisplay(gpDisplay);
+		gpDisplay = NULL;
+	}
+
+	
+	if (gpFile)
+	{
+		fprintf(gpFile, "Log file closed successfully\n");
+		fclose(gpFile);
+		gpFile = NULL;
+	}
+
+}
