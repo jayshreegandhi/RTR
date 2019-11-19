cf33577517e7e8e8fbfbfbc74396b88ebc18cc8e
Jayshree <jayshreeggandhi@gmail.com>
2019-11-19 22:09:11 +0530

    wbgl

---
+++ WebGL/08_3DRotation/Canvas.js
@@ -0,0 +1,627 @@
+// onload function
+var canvas = null;
+var gl = null;
+var bFullscreen = false;
+var canvas_original_width;
+var canvas_original_height;
+var anglePyramid = 0.0;
+var angleCube = 0.0;
+
+const WebGLMacros = {
+    AMC_ATTRIBUTE_POSITION: 0,
+    AMC_ATTRIBUTE_COLOR: 1,
+    AMC_ATTRIBUTE_NORMAL: 2,
+    AMC_ATTRIBUTE_TEXTURE0: 3
+};
+
+var gShaderProgramObject;
+var gVertexShaderObject;
+var gFragmentShaderObject;
+
+var vao_pyramid;
+var vao_cube;
+
+var vbo_position_pyramid;
+var vbo_position_cube;
+
+var vbo_color_pyramid;
+var vbo_color_cube;
+
+var mvpUniform;
+
+var perspectiveProjectionMatrix;
+
+var requestAnimationFrame =
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame;
+
+var cancelAnimationFrame =
+    window.cancelAnimationFrame ||
+    window.webkitCancelRequestAnimationFrame ||
+    window.webkitCancelAnimationFrame ||
+    window.mozCancelRequestAnimationFrame ||
+    window.mozCancelAnimationFrame ||
+    window.oCancelRequestAnimationFrame ||
+    window.oCancelAnimationFrame ||
+    window.msCancelRequestAnimationFrame ||
+    window.msCancelAnimationFrame;
+
+function main() {
+    //S1: Get canvas
+    canvas = document.getElementById("AMC");
+    if (!canvas) console.log("Obtaining Canvas failed!\n");
+    else console.log("Obtaining Canvas successful!\n");
+
+    canvas_original_width = canvas.width;
+    canvas_original_height = canvas.height;
+
+    console.log("Canvas width : " + canvas.width + "Canvas height :" + canvas.height);
+
+    //register keybord and mouse event handler
+    window.addEventListener("keydown", keyDown, false);
+    window.addEventListener("click", mouseDown, false);
+    window.addEventListener("resize", resize, false);
+
+    //initialize WebGL
+    init();
+
+    //start drawing and warming up
+    resize();
+    draw();
+}
+
+function toggleFullScreen() {
+    var fullscreen_element =
+        document.fullscreenElement ||
+        document.webkitFullscreenElement ||
+        document.mozFullScreenElement ||
+        document.msFullscreenElement ||
+        null;
+
+    if (fullscreen_element == null) {
+        if (canvas.requestFullscreen) canvas.requestFullscreen();
+        else if (canvas.mozRequestFullScreen) canvas.mozRequestFullScreen();
+        else if (canvas.webkitFullscreen) canvas.webkitFullscreen();
+        else if (canvas.msRequestFullscreen) canvas.msRequestFullscreen();
+        bFullscreen = true;
+    } else {
+        if (document.exitFullscreen) document.exitFullscreen();
+        else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
+        else if (document.webkitExitFullscreen) document.webkitExitFullscreen();
+        else if (document.msExitFullscreen) document.msExitFullscreen();
+        bFullscreen = false;
+    }
+}
+
+function init() {
+    //S2: Get webgl 2.0 context
+    gl = canvas.getContext("webgl2");
+    if (!gl) {
+        console.log("Failed to get rendering context for WebGL\n");
+        return;
+    } else console.log("Got rendering context for WebGL!\n");
+
+    //set viewport width and height
+    gl.viewportWidth = canvas.width;
+    gl.viewportHeight = canvas.height;
+
+    //vertex shader
+    var vertexShaderSourceCode =
+        "#version 300 es" +
+        "\n" +
+        "in vec4 vPosition;" +
+        "in vec4 vColor;" +
+        "uniform mat4 u_mvp_matrix;" +
+        "out vec4 out_color;" +
+        "void main(void)" +
+        "{" +
+        "   gl_Position = u_mvp_matrix * vPosition;" +
+        "   out_color = vColor;" +
+        "}";
+
+    gVertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(gVertexShaderObject, vertexShaderSourceCode);
+    gl.compileShader(gVertexShaderObject);
+    if (gl.getShaderParameter(gVertexShaderObject, gl.COMPILE_STATUS) == false) {
+        var error = gl.getShaderInfoLog(gVertexShaderObject);
+        if (error.length > 0) {
+            alert("\nVertex Shader compilation log : " + error);
+            uninitialize();
+        }
+    }
+
+    //fragment shader
+    var fragmentShaderSourceCode =
+        "#version 300 es" +
+        "\n" +
+        "precision highp float;" +
+        "in vec4 out_color;" +
+        "out vec4 FragColor;" +
+        "void main(void)" +
+        "{" +
+        "   FragColor = out_color;" +
+        "}";
+
+    gFragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(gFragmentShaderObject, fragmentShaderSourceCode);
+    gl.compileShader(gFragmentShaderObject);
+    if (gl.getShaderParameter(gFragmentShaderObject, gl.COMPILE_STATUS) == false) {
+        var error = gl.getShaderInfoLog(gFragmentShaderObject);
+        if (error.length > 0) {
+            alert("\nFragment Shader compilation log : " + error);
+            uninitialize();
+        }
+    }
+
+    //shader program
+    gShaderProgramObject = gl.createProgram();
+    gl.attachShader(gShaderProgramObject, gVertexShaderObject);
+    gl.attachShader(gShaderProgramObject, gFragmentShaderObject);
+
+    //pre-linking
+    gl.bindAttribLocation(gShaderProgramObject, WebGLMacros.AMC_ATTRIBUTE_POSITION, "vPosition");
+    gl.bindAttribLocation(gShaderProgramObject, WebGLMacros.AMC_ATTRIBUTE_COLOR, "vColor");
+
+    //linking
+    gl.linkProgram(gShaderProgramObject);
+    if (gl.getProgramParameter(gShaderProgramObject, gl.LINK_STATUS) == false) {
+        var error = gl.getProgramInfoLog(gShaderProgramObject);
+        if (error.length > 0) {
+            alert("\nProgram linking log : " + error);
+            uninitialize();
+        }
+    }
+
+    //get uniform location
+    mvpUniform = gl.getUniformLocation(gShaderProgramObject, "u_mvp_matrix");
+
+    //vertices, color, texture, vao, vbo , shader attribs
+    var pyramidVertices = new Float32Array([
+        -1.0,
+        -1.0,
+        1.0,
+        0.0,
+        1.0,
+        0.0,
+        1.0,
+        -1.0,
+        1.0,
+        1.0,
+        -1.0,
+        1.0,
+        0.0,
+        1.0,
+        0.0,
+        1.0,
+        -1.0,
+        -1.0,
+        -1.0,
+        -1.0,
+        -1.0,
+        0.0,
+        1.0,
+        0.0,
+        1.0,
+        -1.0,
+        -1.0,
+        -1.0,
+        -1.0,
+        1.0,
+        0.0,
+        1.0,
+        0.0,
+        -1.0,
+        -1.0,
+        -1.0
+    ]);
+
+    var cubeVertices = new Float32Array([
+        1.0,
+        1.0,
+        -1.0,
+        -1.0,
+        1.0,
+        -1.0,
+        -1.0,
+        1.0,
+        1.0,
+        1.0,
+        1.0,
+        1.0,
+        1.0,
+        -1.0,
+        -1.0,
+        -1.0,
+        -1.0,
+        -1.0,
+        -1.0,
+        -1.0,
+        1.0,
+        1.0,
+        -1.0,
+        1.0,
+        1.0,
+        1.0,
+        1.0,
+        -1.0,
+        1.0,
+        1.0,
+        -1.0,
+        -1.0,
+        1.0,
+        1.0,
+        -1.0,
+        1.0,
+        1.0,
+        1.0,
+        -1.0,
+        -1.0,
+        1.0,
+        -1.0,
+        -1.0,
+        -1.0,
+        -1.0,
+        1.0,
+        -1.0,
+        -1.0,
+        1.0,
+        1.0,
+        -1.0,
+        1.0,
+        1.0,
+        1.0,
+        1.0,
+        -1.0,
+        1.0,
+        1.0,
+        -1.0,
+        -1.0,
+        -1.0,
+        1.0,
+        -1.0,
+        -1.0,
+        1.0,
+        1.0,
+        -1.0,
+        -1.0,
+        1.0,
+        -1.0,
+        -1.0,
+        -1.0
+    ]);
+
+    var pyramidColor = new Float32Array([
+        0.0,
+        1.0,
+        0.0,
+        1.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        1.0,
+        0.0,
+        0.0,
+        1.0,
+        1.0,
+        0.0,
+        0.0,
+        0.0,
+        1.0,
+        0.0,
+        0.0,
+        0.0,
+        1.0,
+        1.0,
+        0.0,
+        0.0,
+        0.0,
+        1.0,
+        0.0,
+        0.0,
+        1.0,
+        0.0,
+        1.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        1.0
+    ]);
+
+    var cubeColor = new Float32Array([
+        1.0,
+        0.0,
+        0.0,
+        1.0,
+        0.0,
+        0.0,
+        1.0,
+        0.0,
+        0.0,
+        1.0,
+        0.0,
+        0.0,
+        0.0,
+        1.0,
+        0.0,
+        0.0,
+        1.0,
+        0.0,
+        0.0,
+        1.0,
+        0.0,
+        0.0,
+        1.0,
+        0.0,
+        0.0,
+        0.0,
+        1.0,
+        0.0,
+        0.0,
+        1.0,
+        0.0,
+        0.0,
+        1.0,
+        0.0,
+        0.0,
+        1.0,
+        0.0,
+        1.0,
+        1.0,
+        0.0,
+        1.0,
+        1.0,
+        0.0,
+        1.0,
+        1.0,
+        0.0,
+        1.0,
+        1.0,
+        1.0,
+        0.0,
+        1.0,
+        1.0,
+        0.0,
+        1.0,
+        1.0,
+        0.0,
+        1.0,
+        1.0,
+        0.0,
+        1.0,
+        1.0,
+        1.0,
+        0.0,
+        1.0,
+        1.0,
+        0.0,
+        1.0,
+        1.0,
+        0.0,
+        1.0,
+        1.0,
+        0.0
+    ]);
+
+    //create vao
+    //triangle
+    vao_pyramid = gl.createVertexArray();
+    gl.bindVertexArray(vao_pyramid);
+
+    //create vbo
+    vbo_position_pyramid = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo_position_pyramid);
+    gl.bufferData(gl.ARRAY_BUFFER, pyramidVertices, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(WebGLMacros.AMC_ATTRIBUTE_POSITION, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(WebGLMacros.AMC_ATTRIBUTE_POSITION);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    vbo_color_pyramid = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo_color_pyramid);
+    gl.bufferData(gl.ARRAY_BUFFER, pyramidColor, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(WebGLMacros.AMC_ATTRIBUTE_COLOR, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(WebGLMacros.AMC_ATTRIBUTE_COLOR);
+
+    //unbind vbo and vao
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    gl.bindVertexArray(null);
+
+    //rectangle
+    vao_cube = gl.createVertexArray();
+    gl.bindVertexArray(vao_cube);
+
+    //create vbo
+    vbo_position_cube = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo_position_cube);
+    gl.bufferData(gl.ARRAY_BUFFER, cubeVertices, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(WebGLMacros.AMC_ATTRIBUTE_POSITION, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(WebGLMacros.AMC_ATTRIBUTE_POSITION);
+    gl.vertexAttrib3f(WebGLMacros.AMC_ATTRIBUTE_COLOR, 1.0, 0.0, 0.0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    vbo_color_cube = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo_color_cube);
+    gl.bufferData(gl.ARRAY_BUFFER, cubeColor, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(WebGLMacros.AMC_ATTRIBUTE_COLOR, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(WebGLMacros.AMC_ATTRIBUTE_COLOR);
+
+    //unbind vbo and vao
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    gl.bindVertexArray(null);
+
+    //depth
+    gl.enable(gl.DEPTH_TEST);
+
+    //gl.enable(gl.CULL_FACE);
+
+    //set clear color
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    perspectiveProjectionMatrix = mat4.create();
+}
+
+function resize() {
+    if (bFullscreen == true) {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    } else {
+        canvas.width = canvas_original_width;
+        canvas.height = canvas_original_height;
+    }
+
+    gl.viewport(0, 0, canvas.width, canvas.height);
+
+    //perspective
+    mat4.perspective(
+        perspectiveProjectionMatrix,
+        45.0,
+        parseFloat(canvas.width) / parseFloat(canvas.height),
+        0.1,
+        100.0
+    );
+}
+
+function draw() {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    gl.useProgram(gShaderProgramObject);
+
+    //matrices
+    var modelViewMatrix = mat4.create();
+    var modelViewProjectionMatrix = mat4.create();
+
+    //triangle
+    //identity
+    mat4.identity(modelViewMatrix);
+    mat4.identity(modelViewProjectionMatrix);
+
+    //transformations
+    mat4.translate(modelViewMatrix, modelViewMatrix, [-1.5, 0.0, -6.0]);
+    mat4.rotateY(modelViewMatrix, modelViewMatrix, degToRad(anglePyramid));
+
+    mat4.multiply(modelViewProjectionMatrix, perspectiveProjectionMatrix, modelViewMatrix);
+
+    gl.uniformMatrix4fv(mvpUniform, false, modelViewProjectionMatrix);
+
+    gl.bindVertexArray(vao_pyramid);
+
+    gl.drawArrays(gl.TRIANGLES, 0, 12);
+
+    gl.bindVertexArray(null);
+
+    //rectangle
+    //identity
+    mat4.identity(modelViewMatrix);
+    mat4.identity(modelViewProjectionMatrix);
+
+    //transformations
+    mat4.translate(modelViewMatrix, modelViewMatrix, [1.5, 0.0, -6.0]);
+    mat4.rotateX(modelViewMatrix, modelViewMatrix, degToRad(angleCube));
+    mat4.rotateY(modelViewMatrix, modelViewMatrix, degToRad(angleCube));
+    mat4.rotateZ(modelViewMatrix, modelViewMatrix, degToRad(angleCube));
+    mat4.scale(modelViewMatrix, modelViewMatrix, [0.75, 0.75, 0.75]);
+    mat4.multiply(modelViewProjectionMatrix, perspectiveProjectionMatrix, modelViewMatrix);
+
+    gl.uniformMatrix4fv(mvpUniform, false, modelViewProjectionMatrix);
+
+    gl.bindVertexArray(vao_cube);
+
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+    gl.drawArrays(gl.TRIANGLE_FAN, 4, 4);
+    gl.drawArrays(gl.TRIANGLE_FAN, 8, 4);
+    gl.drawArrays(gl.TRIANGLE_FAN, 12, 4);
+    gl.drawArrays(gl.TRIANGLE_FAN, 16, 4);
+    gl.drawArrays(gl.TRIANGLE_FAN, 20, 4);
+
+    gl.bindVertexArray(null);
+
+    gl.useProgram(null);
+
+    //animation loop
+    update();
+    requestAnimationFrame(draw, canvas);
+}
+
+function update() {
+    anglePyramid = anglePyramid + 1.0;
+    if (anglePyramid >= 360.0) {
+        anglePyramid = 0.0;
+    }
+
+    angleCube = angleCube - 1.0;
+    if (angleCube <= -360.0) {
+        angleCube = 0.0;
+    }
+}
+
+function uninitialize() {
+    if (vao_pyramid) {
+        gl.deleteVertexArray(vao_pyramid);
+        vao_pyramid = null;
+    }
+
+    if (vbo_position_pyramid) {
+        gl.deleteBuffer(vbo_position_pyramid);
+        vbo_position_pyramid = null;
+    }
+
+    if (vbo_color_pyramid) {
+        gl.deleteBuffer(vbo_color_pyramid);
+        vbo_color_pyramid = null;
+    }
+
+    if (vao_cube) {
+        gl.deleteVertexArray(vao_cube);
+        vao_cube = null;
+    }
+
+    if (vbo_position_cube) {
+        gl.deleteBuffer(vbo_position_cube);
+        vbo = null;
+    }
+
+    if (vbo_color_cube) {
+        gl.deleteBuffer(vbo_color_cube);
+        vbo_color_cube = null;
+    }
+
+    if (gShaderProgramObject) {
+        if (gFragmentShaderObject) {
+            gl.detachShader(gShaderProgramObject, gFragmentShaderObject);
+            gl.deleteShader(gFragmentShaderObject);
+            gFragmentShaderObject = null;
+        }
+        if (gVertexShaderObject) {
+            gl.detachShader(gShaderProgramObject, gVertexShaderObject);
+            gl.deleteShader(gVertexShaderObject);
+            gVertexShaderObject = null;
+        }
+
+        gl.deleteProgram(gShaderProgramObject);
+        gShaderProgramObject = null;
+    }
+}
+
+function keyDown(event) {
+    switch (event.keyCode) {
+        case 27:
+            uninitialize();
+            window.close();
+            break;
+
+        case 70:
+            toggleFullScreen();
+            break;
+    }
+}
+
+function mouseDown() {}
+
+function degToRad(degrees) {
+    var d = Math.PI / 180;
+    return degrees * d;
+}
