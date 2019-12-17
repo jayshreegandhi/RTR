fa12b7ce04583840b76880104333e15e3370a95f
Jayshree <jayshreeggandhi@gmail.com>
2019-12-17 14:35:46 +0530

    mac assignments part 1

---
+++ Mac/02_BlueWindow/BlueWindow.m
@@ -0,0 +1,282 @@
+#import <Foundation/Foundation.h> //stdio.h
+#import <Cocoa/Cocoa.h> // windowing framework
+
+#import <QuartzCore/CVDisplayLink.h>
+
+#import <OpenGL/gl3.h>
+#import <OpenGl/gl3ext.h>
+
+// C Style global function declaration
+CVReturn MyDisplayLinkCallback(CVDisplayLinkRef, const CVTimeStamp *, const CVTimeStamp *, CVOptionFlags, CVOptionFlags *, void *);
+
+//global variables
+FILE *gpFile = NULL;
+
+//interface declarations
+@interface AppDelegate : NSObject <NSApplication, NSWindowDelegate>
+@end
+
+@interface GLView : NSOpenGLView
+@end
+
+//Entry point function
+int main(int argc, const char* argv[])
+{
+    //code
+    NSAutoreleasePool *pPool = [[NSAutoreleasePool alloc]init];
+    NSApp = [NSApplication sharedApplication];
+
+    [NSApp setDelegate:[[AppDelegate alloc]init]];
+
+    [NSApp run];
+
+    [pPool release];
+
+    return(0);
+}
+
+@implementation AppDelegate
+{
+@private
+    NSWindow *window;
+    GLView *glView;
+}
+
+-(void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    //code
+    //log file
+    NSBundle *mainBundle = [NSBundle mainBundle];
+    NSString *appDirName = [mainBundle bundlePath];
+    NSString *parentDirPath = [appDirName stringByDeletingLastPathComponent];
+    NSString *logFileNameWithPath = [NSString stringWithFormat:@"%@/Log.txt",parentDirPath];
+    const char *pszLogFileNameWithPath = [logFileNameWithPath cStringUsingEncoding:NSASCIIStringEncoding];
+    gpFile = fopen(pszLogFileNameWithPath,"w");
+    if(gpFile == NULL)
+    {
+        printf("Can not create log file. \nExiting...\n");
+        [self release];
+        [NSApp terminate:self];
+    }
+    fprintf(gpFile, "Program is started successfully\n");
+
+    //window
+    NSRect win_rect;
+    win_rect = NSMakeRect(0.0, 0.0, 800.0, 600.0);
+
+    //create simple window
+    window = [[NSWindow alloc] initWithContentRect:win_rect
+                                styleMask:NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskMiniaturizable | NSWindowStyleMaskResizable
+                                backing:NSBackingStoreBuffered
+                                defer:NO];
+
+    [window setTitle:@"macOS OpenGl Window"];
+    [window center];
+
+    view =[[MyView alloc] initWithFrame:win_rect];
+    [window setContentView: glView];
+    [window setDelegte: self];
+    [window makeKeyAndOrderFront:self];
+}
+
+-(void)applicationWillTerminate:(NSNotification *)notification
+{
+    //code
+    fprintf(gpFile, "Program Is terminated successfully\n");
+
+    if(gpFile)
+    {
+        fclose(gpFile);
+        gpFile = NULL;
+    }
+}
+
+-(void)windowWillClose:(NSNotification *)notification
+{
+    //code
+    [NSApp terminate:self];
+}
+
+-(void)dealloc
+{
+    //code
+    [view release];
+    [window release];
+    [super dealloc];
+}
+@end
+
+@implementation GLView
+{
+@private
+    CVDisplayLinkRef displayLink;
+}
+
+-(id)initWithFrame:(NSrect)frame
+{
+    self = [super initWithFrame:frame];
+
+    if(self)
+    {
+        [[self window]setContentView: self];
+
+        NSOpenGLPixelFormatAttribute attrs[] =
+        {
+            NSOpenGLPFAOpenGLProfile, NSOpenGLProfileVersion4_1Core,
+            NSOpenGLPFAScreenMask, CGDisplayIDToOpenGLDisplayMask(kCGDirectMainDisplay),
+            NSOpenGLPFANORecovery,
+            NSOpenGLPFAAccelerated,
+            NSOpenGLPFAColorSize, 24,
+            NSOpenGLPFADepthSize, 24,
+            NSOpenGLPFAAlphaSize, 8,
+            NSOpenGLPFADoubleBuffer,
+            0
+        };
+
+        NSOpenGLPixelFormat *pixelFormat = [[[NSOpenGLPixelFormat alloc]initWithAttributes:attrs] autorelease];
+
+        if(pixelFormat == nil)
+        {
+            fprintf(gpFile, "No Valid OpenGL pixel format is available.Exiting...\n");
+            [self release];
+            [NSApp terminate:self];
+        }
+
+        NSOpenGLContext *glContext = [[[NSOpenGLContext alloc]initWithFormat:pixelFormat shareContext:nil] autorelease];
+
+        [self setPixelFormat:pixelFormat];
+        [self setOpenGLContext:glContext];
+    }
+    return(self);
+}
+
+-(CVReturn)getFrameForTime:(const CVTimeStamp *)pOutputTime
+{
+    //code
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc]init];
+
+    [self drawView];
+    [pool release];
+
+    return(kCVReturnSuccess);
+}
+
+-(void)prepareOpenGL
+{
+    //code
+    //OpenGL info
+
+    fprintf(gpFile, "OpenGL Version : %s\n", glGetString(GL_VERSION));
+    fprintf(gpFile, "GLSL Version : %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+    [[self openGLContext]makeCurrentContext];
+
+    GLint swapInt = 1;
+    [[self openGLContext]setValues:&swapInt forParameters:NSOpenGLCPSwapInterval];
+
+    //set bk color
+    glClearColor(0.0f, 0.0f, 1.0f, 0.0f);
+
+    CVDisplayLinkCreateWithActiveCGDisplay(&displayLink);
+    CVDisplayLinkSetOutputCallback(displayLink, &MyDisplayLinkCallback, self);
+    CGLContextObj cglContext = (CGLContextObj)[[self openGLContext]CGLContextObj];
+    CGLPixelFormatObj cglPixelFormat = (CGLPixelFormatObj)[[self pixelFormat]CGLPixelFormatObj];
+
+    CVDisplayLinkSetCurrentCGDisplayFromOpenGLContext(displayLink, cglContext, cglPixelFormat);
+    CVDisplayLinkStart(displayLink);
+}
+
+-(void)reshape
+{
+    //code
+    CGLLockContext((CGLContextObj)[[self openGLContext]CGLContextObj]);
+    NSRect rect = [self bounds];
+
+    GLfloat width = rect.size.width;
+    GLfloat height = rect.size.height;
+
+    if(height == 0)
+        height = 1;
+
+    glViewPort(0, 0, (GLsizei)width, (GLsizei)height);
+
+    CGLUnlockContext((CGLContextObj)[[self openGLContext]CGLContextObj]);
+}
+
+-(void)drawRect:(NSRect)dirtyRect
+{
+    //code
+    [self drawView];
+}
+
+-(void)drawView
+{
+    [[self openGLContext]makeCurrentContext];
+
+    CGLLockContext((CGLContextObj)[[self openGLContext]CGLContextObj]);
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    CGLFlushDrawable((CGLContextObj)[[self openGLContext]CGLContextObj]);
+    CGLUnlockContext((CGLContextObj)[[self openGLContext]CGLContextObj]);
+}
+
+-(BOOL)acceptFirstResponder
+{
+    //code
+    [[self window]makeFirstresponder:self];
+    return(YES);
+}
+
+-(void)keyDown:(NSEvent *)theEvent
+{
+    //code
+    int key = (int)[[theEvent characters]characterAtIndex:0];
+
+    switch(key)
+    {
+        case 27:
+            [self release];
+            [NSApp terminate:self];
+            break;
+
+        case 'F':
+        case 'f':
+            [[self window]toggleFullScreen:self];
+            break;
+
+        default:
+            break;
+    }
+}
+
+-(void)mouseDown:(NSEvent *)theEvent
+{
+    //code
+}
+
+-(void)mouseDragged:(NSEvent *)theEvent
+{
+    //code
+}
+
+-(void)rightMouseDown:(NSEvent *)theEvent
+{
+    //code
+}
+
+-(void)dealloc
+{
+    //code
+    CVDisplayLinkStop(displayLink);
+    CVDisplayLinkRelease(displayLink);
+
+    [super dealloc];
+}
+@end
+
+CVReturn MyDisplayLinkCallback(CVDisplayLinkRef displayLink, const CVTimeStamp *pNow, const CVTimeStamp *pOutputTime, CVOptionFlags flagsIn, CVOptionFlags *pFlagsOut, void *pDisplayLinkContext)
+{
+    CVReturn result = [(GLView *)pDisplayLinkContext getFrameForTime:pOutputTime];
+    return(result);
+}
